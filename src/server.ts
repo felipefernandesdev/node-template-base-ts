942e87667ad62a61ac8771af2ca90db22790590f
Felipe Sousa Developer <fesousalocutor@gmail.com>
2022-07-24 06:02:32 -0300

    server with cluster

--- src/server.ts
+++ src/server.ts
@@ -12,13 +12,13 @@ app.use(express.json());
 app.use(logger('combined'));
 
 if (cluster.isPrimary) {
-  for (let i = 0; i < numCpus; i++) cluster.fork();
+    for (let i = 0; i < numCpus; i++) cluster.fork();
 } else if (cluster.isWorker) {
-  app.get('/', (req: Request, res: Response) => {
-    res.json({
-      msg: 'Welcome API Rest, created by Felipe Fernandes!'
+    app.get('/', (req: Request, res: Response) => {
+        res.json({
+            msg: 'Welcome API Rest, created by Felipe Fernandes!',
+        });
+        console.log('cluster ' + cluster.worker.process.pid + ' responded \n');
     });
-    console.log('cluster ' + cluster.worker.process.pid + ' responded \n');
-  });
-  app.listen(port, () => console.log(`API is running at port 3000`));
+    app.listen(port, () => console.log(`API is running at port 3000`));
 }
