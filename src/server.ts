7eb7ffdc0466faa14c62875aa38d7fd4d600cf5e
Felipe Sousa Developer <fesousalocutor@gmail.com>
2022-07-24 05:57:18 -0300

    config cluster and cpus app usage

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,24 @@
 import 'dotenv/config'; // Configuration DOTENV
 import logger from 'morgan';
 import express, { Request, Response } from 'express';
+import cluster from 'cluster';
+import { cpus } from 'os';
 
+const numCpus = cpus().length;
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(logger('combined'));
 
-app.get('/', (req: Request, res: Response) => {
-  res.json({
-    msg: 'Welcome API Rest, created by Felipe Fernandes!'
+if (cluster.isPrimary) {
+  for (let i = 0; i < numCpus; i++) cluster.fork();
+} else if (cluster.isWorker) {
+  app.get('/', (req: Request, res: Response) => {
+    res.json({
+      msg: 'Welcome API Rest, created by Felipe Fernandes!'
+    });
+    console.log('cluster ' + cluster.worker.process.pid + ' responded \n');
   });
-});
-
-app.listen(port, () => console.log(`API is running at port 3000`));
+  app.listen(port, () => console.log(`API is running at port 3000`));
+}
